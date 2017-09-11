b322740091400f2601447817f2926a03413cbb3c
Nikola <bigmecoprog@gmail.com>
2017-09-11 19:14:49 +0300

    get url VK

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,16 @@
+import javafx.event.ActionEvent;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Controller {
+    public void Sjitat(ActionEvent actionEvent) throws IOException {
+        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream("C:/Users/bigmeco/Documents/ExcelVK/sima_19_spiski.xlsx"));
+        System.out.println(   myExcelBook.getSheetAt(0).getRow(1).getCell(0).getHyperlink().getAddress());
+
+    }
 }
+//
